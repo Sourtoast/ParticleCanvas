aab97822fcadcdf9601ad9f0c584448fc52f8f7e
Wiktor Buliński <bul.wiktor@gmail.com>
2023-06-16 12:31:04 +0200

    refactor: code grooming

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const canvasElement = document.querySelector("#particles")
-const ctx = canvasElement.getContext("2d")
-const parentStyles = getComputedStyle(canvasElement.parentElement)
+const canvasElement = document.querySelector("#particles");
+const ctx = canvasElement.getContext("2d");
+const parentStyles = getComputedStyle(canvasElement.parentElement);
 
-const {backgroundColor, color: foregroundColor} = parentStyles
+const { backgroundColor, color: foregroundColor } = parentStyles;
 
 function resizeCanvasToMatchParent() {
-    canvasElement.width = canvasElement.parentElement.clientWidth
-    canvasElement.height = canvasElement.parentElement.clientHeight
+  canvasElement.width = canvasElement.parentElement.clientWidth;
+  canvasElement.height = canvasElement.parentElement.clientHeight;
 }
-
-resizeCanvasToMatchParent()
 
 function drawParticle() {
-    ctx.beginPath()
-    ctx.arc(canvasElement.width / 2, canvasElement.height / 2, 2, 0, Math.PI * 2)
-    ctx.fillStyle = foregroundColor
-    console.log(foregroundColor)
-    ctx.fill()
-    ctx.closePath()
+  ctx.beginPath();
+  ctx.arc(canvasElement.width / 2, canvasElement.height / 2, 2, 0, Math.PI * 2);
+  ctx.fillStyle = foregroundColor;
+  console.log(foregroundColor);
+  ctx.fill();
+  ctx.closePath();
 }
 
-drawParticle()
+function setup() {
+  resizeCanvasToMatchParent();
+  window.addEventListener("resize", resizeCanvasToMatchParent);
+}
+
+setup();
+drawParticle();
