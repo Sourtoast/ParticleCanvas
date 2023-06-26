39f484cdf7fa75e099f7e463ec3342bdfd5a6f50
Wiktor Buliński <wiktor.b@masterborn.com>
2023-06-26 10:23:48 +0200

    feat: added drawing lines to moue

--- index.js
+++ index.js
@@ -9,12 +9,25 @@ function resizeCanvasToMatchParent() {
   canvasElement.height = canvasElement.parentElement.clientHeight;
 }
 
+function handleMouseMove(e) {
+  mousePosition.x = e.offsetX;
+  mousePosition.y = e.offsetY;
+}
+
+function handleMouseOut() {
+  mousePosition.x = undefined;
+  mousePosition.y = undefined;
+}
+
 function setup() {
   const pixelRatio = window.devicePixelRatio;
   ctx.scale(pixelRatio, pixelRatio);
   resizeCanvasToMatchParent();
   window.addEventListener("resize", resizeCanvasToMatchParent);
+  canvasElement.addEventListener("mousemove", handleMouseMove);
+  canvasElement.addEventListener("mouseout", handleMouseOut);
 }
+
 setup();
 
 const particles = Array(
@@ -23,7 +36,10 @@ const particles = Array(
   .fill()
   .map(() => new Particle({ ctx }));
 
+const mousePosition = { x: null, y: null };
+
 let lastFrameTimestamp = 0;
+
 async function draw(timestamp) {
   const timestampInSeconds = timestamp / 1000;
   const elapsedSinceLastFrame = timestampInSeconds - lastFrameTimestamp;
@@ -39,11 +55,19 @@ async function draw(timestamp) {
     );
 
     nearParticles.forEach((nearParticle) => particle.drawLineTo(nearParticle));
+
+    const distanceToMouse = particle.distanceTo(mousePosition);
+    // This check can be confusing. If any of coordinates for mouse position is null (mouse is not on canvas) distanceToMouse is NaN which is falsy.
+    // Zero is also falsy, but it's an edge case which is not relevant
+    if (!!distanceToMouse && distanceToMouse <= MAX_CONNECTION_LENGTH)
+      particle.drawLineTo(mousePosition);
+
     particle.checkedForConnections = true;
 
     particle.draw();
     particle.requestNewPosition(elapsedSinceLastFrame);
   });
+
   requestAnimationFrame(draw);
 }
 
